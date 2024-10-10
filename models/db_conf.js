5c099fb73dce955ac543865278a58947dbbd296a
wassim <wassim.naai@student.vinci.be>
2024-10-10 15:21:00 +0200

    first commit

--- models/db_conf.js
+++ models/db_conf.js
@@ -3,4 +3,6 @@
 const db = require('better-sqlite3')('/opt/web1_23-24/db/exoplanets.db', { verbose: console.log });
 // const db = require('better-sqlite3')('/home/olivier/exoplanets.db', { verbose: console.log });
 
+
+
 module.exports = db;
